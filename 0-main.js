6a4cd89d86608bf4d1b67766ce1b73ecfd5674f4
lemonRoof <137109444+lemonRoof@users.noreply.github.com>
2023-06-27 09:46:11 +0000

    Compelet task 0

--- 0-main.js
+++ 0-main.js
@@ -1,3 +1,4 @@
-import { taskFirst, taskNext } from './0x00-ES6_basic/0-constants.js';
+import getResponseFromAPI from "./0x01-ES6_promise/0-promise.js";
 
-console.log(`${taskFirst()} ${taskNext()}`);
+const response = getResponseFromAPI();
+console.log(response instanceof Promise);
