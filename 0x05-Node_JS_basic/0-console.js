19a3f80d25c41f26a59dd06e0abb320f989eac7c
lemonRoof <137109444+lemonRoof@users.noreply.github.com>
2023-08-23 08:58:05 +0000

    Task

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -4,8 +4,6 @@
  * @author Bezaleel Olakunori <https://github.com/B3zaleel>
  */
 const displayMessage = (msg) => {
-    console.log(msg);
-  };
-  
-  module.exports = displayMessage;
-  
+  console.log(msg);
+};
+module.exports = displayMessage;
