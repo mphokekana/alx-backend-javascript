dda18827e88519ea677a69e4440f90a5ac7b6f39
Mpho Kekana <111887473+mphokekana@users.noreply.github.com>
2023-10-10 09:06:07 +0000

    Complete task 3

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -2,4 +2,3 @@ function getStudentsByLocation(students, city) {
   return students.filter((student) => student.location === city);
 }
 export default getStudentsByLocation;
-
--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,4 @@
 const getStudentIdsSum = (students) => students.reduce((prevVal, cur) => prevVal + cur.id, 0);
 
 export default getStudentIdsSum;
+
