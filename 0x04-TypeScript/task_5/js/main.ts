d8bd207281ca45f77f323a75ac9c8c9da47245e9
Mpho Kekana <111887473+mphokekana@users.noreply.github.com>
2023-10-10 09:16:26 +0000

    Complete Task

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -13,4 +13,3 @@ export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits):
 export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
   return { credits: subject1.credits + subject2.credits } as MinorCredits;
 }
-
